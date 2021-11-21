c5cb3815bd313b0bb815bc8e694a3459e6411091
tenOndro <ondriga.patrik@gmail.com>
2021-11-21 23:08:21 +0100

    Store whole project

--- app/src/main/java/com/example/postcutter/customViews/RectangleView.java
+++ app/src/main/java/com/example/postcutter/customViews/RectangleView.java
@@ -2,6 +2,7 @@ package com.example.postcutter.customViews;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -14,14 +15,43 @@ public class RectangleView extends FrameLayout {
 
     private MyRectangle rectangle = null;
 
-    private final View topLeftCorner = findViewById(R.id.rectangle_topLeftCorner);
-    private final View topRightCorner = findViewById(R.id.rectangle_topRightCorner);
-    private final View bottomLeftCorner = findViewById(R.id.rectangle_bottomLeftCorner);
-    private final View bottomRightCorner = findViewById(R.id.rectangle_bottomRightCorner);
+    private View innerRectangle;
+    private View topLeftCorner;
+    private View topRightCorner;
+    private View bottomLeftCorner;
+    private View bottomRightCorner;
 
     public RectangleView(Context context, AttributeSet attrs) {
         super(context, attrs);
         inflate(getContext(), R.layout.rectangle_view, this);
+    }
+
+    //TODO debug
+    private void moveTop(MotionEvent event) {
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("Action_down position==========================");
+                System.out.println(event.getX() + ", " + event.getY());
+                break;
+//            case MotionEvent.ACTION_MOVE:
+//                float movedY = event.getY();
+//                float distanceY = movedY - this.downTopY;
+//                float newY = this.topLine.getY() + distanceY;
+//                if(suggestLine) {
+//                    float tmp = this.nearBreakPoint(newY, this.breakPoints);
+//                    float tmp = this.nearBreakPoint(newY, this.breakPoints);
+//                    if (tmp != newY) {
+//                        newY = tmp - this.lineWidth;
+//                    }
+//                }
+//                setTop(newY);
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                int cutPosition = (int) this.topLine.getY() - this.imageRectangle.getCornerA().getY() + this.lineWidth;
+//                this.cutRectangle.getCornerA().setY(
+//                        mapping(this.imageRectangle.getHeight(), this.originalHeight, cutPosition)
+//                );
+        }
     }
 
     public void setRectangle(Coordinate cornerA, Coordinate cornerB) {
@@ -32,10 +62,33 @@ public class RectangleView extends FrameLayout {
         cornerB.setX(cornerB.getX() - shift);
         cornerB.setY(cornerB.getY() - shift);
         this.rectangle = MyRectangle.createRectangle(cornerA, cornerB);
+
+        prepairViews();
+    }
+
+    private void prepairViews() {
+        innerRectangle = findViewById(R.id.rectangle_innerRectangle);
+        topLeftCorner = findViewById(R.id.rectangle_topLeftCorner);
+        topRightCorner = findViewById(R.id.rectangle_topRightCorner);
+        bottomLeftCorner = findViewById(R.id.rectangle_bottomLeftCorner);
+        bottomRightCorner = findViewById(R.id.rectangle_bottomRightCorner);
+
+        //TODO debug
+        this.bottomRightCorner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                moveTop(event);
+                return true;
+            }
+        });
     }
 
     public MyRectangle getRectangle() {
         return rectangle;
+    }
+
+    public View getInnerRectangle() {
+        return innerRectangle;
     }
 
     public View getTopLeftCorner() {
