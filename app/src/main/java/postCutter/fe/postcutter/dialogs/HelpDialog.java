d3785dca57a31d085d7e234728341b3d7534bb9c
tenOndro <ondriga.patrik@gmail.com>
2022-05-07 18:39:13 +0200

    Add skip button for tutorial.

--- app/src/main/java/postCutter/fe/postcutter/dialogs/HelpDialog.java
+++ app/src/main/java/postCutter/fe/postcutter/dialogs/HelpDialog.java
@@ -1,12 +1,13 @@
 package postCutter.fe.postcutter.dialogs;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.AppCompatButton;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -25,8 +26,6 @@ public class HelpDialog {
 
     private TextView text;
     private ImageView image;
-    private TextView counter;
-    private AppCompatButton button;
 
     public HelpDialog(Activity activity, List<Help> helps) {
         this.activity = activity;
@@ -35,30 +34,38 @@ public class HelpDialog {
         LayoutInflater inflater = activity.getLayoutInflater();
         this.dialogBuilder = new MaterialAlertDialogBuilder(activity)
                 .setCancelable(false)
-                .setTitle(R.string.help_title)
+                .setTitle(activity.getText(R.string.help_title) + " 0/" + helps.size())
+                .setPositiveButton(activity.getResources().getString(R.string.next), null)
+                .setNegativeButton(activity.getResources().getString(R.string.skip), null)
                 .setView(inflater.inflate(R.layout.help_dialog, null));
     }
 
     public void show() {
-        this.dialog = this.dialogBuilder.show();
+        this.dialog = this.dialogBuilder.create();
 
+        this.dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                next();
+            }
+        });
+
+        dialog.show();
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(e -> next());
         this.text = dialog.findViewById(R.id.help_dialog__text);
         this.image = dialog.findViewById(R.id.help_dialog__image);
-        this.counter = dialog.findViewById(R.id.help_dialog__counter);
-        this.button = dialog.findViewById(R.id.help_dialog__button);
-        this.button.setOnClickListener(e -> next());
-
-        this.next();
     }
 
     private void next() {
         if (helps.size() > index) {
             Help help = helps.get(index++);
-            this.counter.setText(index + "/" + helps.size());
+            dialog.setTitle(activity.getText(R.string.help_title) + " " + index + "/" + helps.size());
             this.text.setText(help.getText());
             Glide.with(activity).asGif().load(help.getImage()).into(this.image);
-            if (index == helps.size()){
-                this.button.setText(activity.getResources().getString(R.string.close));
+            if (index == helps.size()) {
+                dialog.getButton(DialogInterface.BUTTON_POSITIVE).setText(activity.getResources().getString(R.string.close));
+                dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setVisibility(View.GONE);
             }
         } else {
             dialog.dismiss();
